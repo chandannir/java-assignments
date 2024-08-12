557f4c9cd4f18b41b0d82996f80cf240e8a6d138
Chandan Nir <chandannir82@gmail.com>
2024-08-12 16:01:22 -0400

    basic class creation and manipulation

---
+++ CustomerTestProgram.java
@@ -0,0 +1,19 @@
+public class CustomerTestProgram {
+    public static void main(String[] args) {
+        Customer c;
+
+        c = new Customer();
+        c.name = "Bob";
+        c.age = 27;
+        c.money = 50.0F;
+        System.out.println(c.name);
+        System.out.println(c.age);
+        System.out.println(c.money);
+
+
+
+
+
+
+    }
+}
