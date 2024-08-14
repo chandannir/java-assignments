90b49f0023df78f6528d805a3ff0aed327f0cea8
Chandan Nir <chandannir82@gmail.com>
2024-08-14 13:29:38 -0400

    Class, objects and manipulation

---
+++ CustomerConstructorTestProgram.java
@@ -0,0 +1,21 @@
+public class CustomerConstructorTestProgram {
+    public static void main(String[] args) {
+        Customer c1, c2, c3;
+
+        c1 = new Customer();
+        c1. name = "Bob";
+        c1. age = 17;
+        c1. money = 10;
+
+        c2 = new Customer();
+        c2. name = "Dottie";
+        c2. age = 3;
+        c2. money = 0;
+
+
+        c3 = new Customer();
+
+        System.out.println();
+    }
+
+}
