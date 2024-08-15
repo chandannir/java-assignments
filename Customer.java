1b84fde3e317a611744edfac05b220830e792c95
Chandan Nir <chandannir82@gmail.com>
2024-08-15 19:32:26 -0400

    add method

--- Customer.java
+++ Customer.java
@@ -1,17 +1,22 @@
 public class Customer {
     String name, name2;
-    int age,age2,cfcElo;
+    int age, age2, cfcElo;
     float money;
 
-    public Customer(String initName, int initAge, float initMoney){
+    public Customer(String initName, int initAge, float initMoney) {
         name = initName;
         age = initAge;
         money = initMoney;
     }
 
+    public int computeFee() {
+
+        if (age >= 18 && age <= 64) {
+            money = money - 12;
+
+        }
 
 
-
-
+    }
 
 }
--- CustomerConstructorTestProgram.java
+++ CustomerConstructorTestProgram.java
@@ -1,23 +1,16 @@
 public class CustomerConstructorTestProgram {
     public static void main(String[] args) {
-        Customer c1, c2, c3;
+        Customer c1, c2, c3, c4;
 
         c1 = new Customer("Bob", 17, 10);
-        //c1. name = "Bob";
-        //c1. age = 17;
-        //. money = 10;
-
         c2 = new Customer("Dottie", 3, 0 );
-        //    c2. name = "Dottie";
-    //        c2. age = 3;
-    //        c. money = 0;
-
-
         c3 = new Customer("Jane", 0, 0 );
+        c4 = new Customer("null", 0, 0);
 
         System.out.println("Bob looks like this: " + c1.name + ", " + c1.age + ",  " + c1.money);
         System.out.println("Dottie looks like this: " +  c2.name + ", " + c2.age + ", " + c2.money);
-        System.out.println("Customer 3 looks like this: " + c3.name + ", " + c3.age + ", "  + c3.money);
+        System.out.println("Jane 3 looks like this: " + c3.name + ", " + c3.age + ", "  + c3.money);
+        System.out.println("Customer 4 looks like this: " + c2.name + ", " + c4.age + ", "  + c4.money);
     }
 
 }
